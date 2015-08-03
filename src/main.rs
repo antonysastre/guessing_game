43fa20624f9394ccecc254f54e324bdffea7f89b
Antony <antony@thenewconcept.com>
2015-08-03 20:41:56 +0200

    Randomize a number.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
-  println!("Hello, init!");
+  println!("Guess a number!");
+
+  let secret_number = rand::thread_rng().gen_range(1, 11);
+
+  println!("The secret number is: {}.", secret_number);
 }
