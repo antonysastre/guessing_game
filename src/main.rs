42381db88d2be2a964752fa3998aaede4cc87741
Antony <antony@thenewconcept.com>
2015-08-03 20:47:52 +0200

    Typecast string and compare numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -8,13 +9,27 @@ fn main() {
 
   let secret_number = rand::thread_rng().gen_range(1, 11);
 
-  println!("Input your guess:");
+  println!("I'm thinking of a number.");
 
-  let mut guess = String::new();
+    println!("Input your guess:");
 
-  io::stdin().read_line(&mut guess)
-    .ok()
-    .expect("Failed to read line.");
+    let mut guess = String::new();
 
-  println!("The secret number is: {}.", secret_number);
+    io::stdin().read_line(&mut guess)
+      .ok()
+      .expect("Failed to read line.");
+
+    let guess: u32 = guess.trim().parse()
+      .ok()
+      .expect("Please type a number!");
+
+    println!("The secret number was: {}.", secret_number);
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => println!("You win!")
+    }
 }
